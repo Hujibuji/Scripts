00b605eb4f120267784e324c68f39d3b9fa42b32
kalloware <145567766+Hujibuji@users.noreply.github.com>
2026-01-01 21:54:17 +0800

    Add support for Speed Clicker game

--- Loader.lua
+++ Loader.lua
@@ -2,6 +2,7 @@ local SupportedGames = {
     [137969408767471] = "https://raw.githubusercontent.com/Hujibuji/Scripts/refs/heads/main/Make%20A%20Army.lua",
     [2753915549] = "https://raw.githubusercontent.com/Hujibuji/Scripts/refs/heads/main/Blox%20Fruits%20Hub.lua",
     [109983668079237] = "https://raw.githubusercontent.com/Hujibuji/Scripts/refs/heads/main/Sab%20Hub.lua",
+    [140374914197602] = "https://raw.githubusercontent.com/Hujibuji/Scripts/refs/heads/main/Speed%20Clicker.lua",
 }
 
 local currentPlaceId = game.PlaceId

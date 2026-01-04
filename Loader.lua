d20858f6c638470cbba1fe2a8aac70e7e5c0c346
kalloware <145567766+Hujibuji@users.noreply.github.com>
2026-01-04 16:14:21 +0800

    Update script ID for 99 Nights In Da Forest

--- Loader.lua
+++ Loader.lua
@@ -3,7 +3,7 @@ local SupportedGames = {
     [2753915549] = "https://raw.githubusercontent.com/Hujibuji/Scripts/refs/heads/main/Blox%20Fruits%20Hub.lua",
     [109983668079237] = "https://raw.githubusercontent.com/Hujibuji/Scripts/refs/heads/main/Sab%20Hub.lua",
     [140374914197602] = "https://raw.githubusercontent.com/Hujibuji/Scripts/refs/heads/main/Speed%20Clicker.lua",
-    [79546208627805] = "https://raw.githubusercontent.com/Hujibuji/Scripts/refs/heads/main/99%20Nights%20In%20Da%20Forest.lua",
+    [126509999114328] = "https://raw.githubusercontent.com/Hujibuji/Scripts/refs/heads/main/99%20Nights%20In%20Da%20Forest.lua",
 }
 
 local currentPlaceId = game.PlaceId
